e0918328ec603a905bf3930b0d3d0ca3bb1f9a36
kartikeya-dewal <kartikeya.dewal@gmail.com>
2017-12-29 19:36:07 +1100

    messages page skeleton

--- src/pages/tabs/tabs.module.ts
+++ src/pages/tabs/tabs.module.ts
@@ -3,16 +3,18 @@ import { IonicPageModule } from 'ionic-angular';
 import { TabsPage } from './tabs';
 import { ProfilePage } from '../profile/profile';
 import { SetupProfilePage } from '../setup-profile/setup-profile';
+import { MessagesPage } from '../messages/messages';
 
 @NgModule({
   declarations: [
     TabsPage,
     ProfilePage,
-    SetupProfilePage
+    SetupProfilePage,
+    MessagesPage
   ],
   imports: [
     IonicPageModule.forChild(TabsPage),
   ],
-  entryComponents: [ProfilePage, SetupProfilePage]
+  entryComponents: [ProfilePage, SetupProfilePage, MessagesPage]
 })
 export class TabsPageModule { }
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,13 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage } from 'ionic-angular';
 import { ProfilePage } from '../profile/profile';
 import { SetupProfilePage } from '../setup-profile/setup-profile';
-
-/**
- * Generated class for the TabsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { MessagesPage } from '../messages/messages';
 
 @IonicPage()
 @Component({
@@ -19,6 +13,7 @@ export class TabsPage {
 
   tab1Root = ProfilePage;
   tab2Root = SetupProfilePage;
+  tab3Root = MessagesPage;
 
   constructor() {
   }
